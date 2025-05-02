3d986b723190d72af34fdbc9b69e60b4eacdd7e0
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-05-02 23:39:16 +0700

    deploy 0.0.19 02/05/25

--- src/app/components/templates/HeroEventRestart25.jsx
+++ src/app/components/templates/HeroEventRestart25.jsx
@@ -6,9 +6,9 @@ import Link from "next/link"
 
 export default function HeroEventRestart2025() {
     return(
-        <div className="relative pb-10 pt-64 gap-5 flex flex-col items-center w-full bg-[#061E71] overflow-hidden lg:pt-[542px]">
+        <div className="relative pb-10 pt-64 gap-5 flex flex-col items-center w-full bg-[#020203] overflow-hidden lg:pt-[542px]">
             {/* Welcoming Back */}
-            <div className="absolute flex flex-col items-center gap-4 top-9 left-1/2 -translate-x-1/2 z-20 lg:top-20">
+            <div className="absolute flex flex-col items-center gap-4 top-9 left-1/2 -translate-x-1/2 z-20 lg:top-18">
                 <Image
                 className="flex max-w-40 lg:max-w-72"
                 src={"https://static.wixstatic.com/media/02a5b1_f73718a961f344cd80016aa1f5522fb6~mv2.webp"}
@@ -26,7 +26,7 @@ export default function HeroEventRestart2025() {
             </div>
 
             {/* Overlay */}
-            <div className="overlay absolute left-0 bottom-0 w-full h-[320px] bg-gradient-to-t from-10% from-[#061E71]/80 via-50% via-[#061E71] to-100% to-[#061E71]/0 z-40"/>
+            <div className={`overlay absolute left-1/2 -translate-x-1/2 top-58 w-[800px] h-[502px] z-40 bg-[#3417E3] rounded-full blur-2xl lg:w-[1752px] lg:h-[1058px] lg:top-[480px]`}/>
 
             {/* Brand Representative */}
             <Image
@@ -54,12 +54,13 @@ export default function HeroEventRestart2025() {
             src={"https://static.wixstatic.com/shapes/02a5b1_36cd04ea04ef4d3d9a0a1f3fc0acd538.svg"}
             alt="Ornament Logo Sevenpreneur"
             width={200}
-            height={200}/>
+            height={200}
+            layout="raw"/>
 
             {/* Background Image */}
             <Image
-            className="background absolute flex opacity-55 -top-32 mix-blend-multiply object-cover h-full"
-            src={"https://static.wixstatic.com/media/02a5b1_fd26f9061e30444ba46ab740e9e44d26~mv2.webp"}
+            className="background-design absolute flex max-w-[420px] top-8 left-1/2 -translate-x-1/2 lg:max-w-[1200px] lg:top-16"
+            src={"https://static.wixstatic.com/shapes/02a5b1_8a2fc0c9c99a4bc8a4366aab12450a53.svg"}
             alt="Background Hero"
             width={2440}
             height={2440}/>
