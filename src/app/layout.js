a0972ed8b635f9e0cf3c98175f1d98284992cb41
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-05-03 17:43:45 +0700

    deploy 0.0.20  03/05/25

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Plus_Jakarta_Sans, Mona_Sans } from "next/font/google";
 import TopNavbar from "./components/templates/TopNavbarRestart25";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import BottomFooter from "./components/templates/BottomFooter";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -37,6 +37,7 @@ export function generateMetadata() {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <GoogleTagManager gtmId="GTM-MZJSK4G3"/>
       <body className={`${monaSans.variable} ${plusJakartaSans.variable} antialiased`}>
         <TopNavbar/>
         {children}
