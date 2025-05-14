3bb5fedf773fdf4594ef1226b2b746baafd982b8
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-05-14 12:55:29 +0700

    deploy 0.0.27 14/05/25

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import "./globals.css";
 import { Plus_Jakarta_Sans, Mona_Sans } from "next/font/google";
-import TopNavbar from "./components/templates/TopNavbarRestart25";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
+import TopNavbar from "./components/templates/TopNavbarRestart25";
 import BottomFooter from "./components/templates/BottomFooter";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -43,6 +44,29 @@ export default function RootLayout({ children }) {
         {children}
         <BottomFooter/>
         <GoogleAnalytics gaId="G-J8V0HJXTSM"/>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '725830956549523');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=725830956549523&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
