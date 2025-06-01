1e5b81861639aa8850a86b3c407fcd018532eb78
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-01 19:11:43 +0700

    adding privacy policy page. Related: SVP-18 SVP-19

--- src/app/(www)/www/layout.js
+++ src/app/(www)/www/layout.js
@@ -1,9 +1,10 @@
-import "./globals.css";
+import "../../globals.css"
 import { Plus_Jakarta_Sans, Mona_Sans } from "next/font/google";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
-import TopNavbar from "./components/templates/TopNavbarRestart25";
-import BottomFooter from "./components/templates/BottomFooter";
+import localFont from "next/font/local"
+import TopNavbar from "../../components/templates/TopNavbarRestart25";
+import BottomFooter from "../../components/templates/BottomFooter";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
@@ -14,6 +15,48 @@ const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
+
+const openSauceOne = localFont({
+  variable: "--font-open-sauce-one",
+  display: "swap",
+  src: [
+    {
+      path: "../../fonts/OpenSauceOne-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../fonts/OpenSauceOne-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ]
+})
 
 export function generateMetadata() {
   const BASE_URL = "https://www.sevenpreneur.com"
@@ -39,7 +82,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <GoogleTagManager gtmId="GTM-MZJSK4G3"/>
-      <body className={`${monaSans.variable} ${plusJakartaSans.variable} antialiased`}>
+      <body className={`${monaSans.variable} ${plusJakartaSans.variable} ${openSauceOne.variable} antialiased`}>
         <TopNavbar/>
         {children}
         <BottomFooter/>
