24a4467ee05e69e4dde28e69c7357b6d39bd47e0
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-06-05 12:00:00 +0700

    Integrate Prisma ORM
    Prisma is added into the tRPC context.
    
    Related: SVP-25

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,9 @@
 import { HydrateClient, trpc } from "../../../trpc/server";
 import { ClientGreeting } from "./client-greeting";
 
-export default function AdminHomePage() {
+export default async function AdminHomePage() {
   // This is for server-side rendering (on first page load)
-  void trpc.getHello.prefetch({
+  await trpc.getHello.prefetch({
     text: "brothers and sisters",
   });
 
