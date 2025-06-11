cb4b72f324b6d9726c0eae72cfdc3d8fb8591a90
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-11 11:53:20 +0700

    added dr. tirta as speakers

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "static.wixstatic.com",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "yt3.googleusercontent.com",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "media.licdn.com",
-                port: "",
-            },
-            {
-                protocol: "https",
-                hostname: "thumbor.prod.vidiocdn.com",
-                port: "",
-            }
-        ]
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "tskubmriuclmbcfmaiur.supabase.co",
+        port: "",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["www.example.com:3000"],
     },
-    experimental: {
-      serverActions: {
-        allowedOrigins: [
-          // TODO: Add local/production URL automatically
-          'www.example.com:3000'
-        ]
-      }
-    }
+  },
 };
 
 export default nextConfig;
--- src/app/components/templates/SpeakersLineUpRestart25.jsx
+++ src/app/components/templates/SpeakersLineUpRestart25.jsx
@@ -1,58 +1,70 @@
-"use client"
-import SectionTitleRestart25 from "../elements/SectionTitleRestart25"
-import SpeakerItemRestart25 from "../elements/SpeakerItemRestart25"
+"use client";
+import SectionTitleRestart25 from "../elements/SectionTitleRestart25";
+import SpeakerItemRestart25 from "../elements/SpeakerItemRestart25";
 
 export default function SpeakersLineUpRestart25() {
+  const speakerData = [
+    {
+      name: "Raymond Chin",
+      title: "Founder Sevenpreneur",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_dad1b41047da42bd92dd2eba42418c37~mv2.webp",
+    },
+    {
+      name: "Basuki Tjahaja Purnama (Ahok)",
+      title: "Former Governor of Jakarta 2014 - 2017",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_7933f447a8d443359f3c9d07c6a40a08~mv2.webp",
+    },
+    {
+      name: "Denny Santoso",
+      title: "Founder & CEO BrainBoost",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_e9e8d63f9e6d423ea7dab593d6009207~mv2.webp",
+    },
+    {
+      name: "dr.Tirta",
+      title: "General Practitioner & Creativepreneur",
+      image:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur//dr-tirta.webp",
+    },
+    {
+      name: "Bhima Yudhistira",
+      title: "Executive Director CELIOS",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_a015ebc361e5456a9cc5c72f569106da~mv2.webp",
+    },
+    {
+      name: "Irzan Raditya",
+      title: "CEO & Co-Founder Kata.ai",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_2ecea6c5c5b34534bd47f20a62f2c7b8~mv2.webp",
+    },
+    {
+      name: "And more speakers",
+      title: "will be revealed",
+      image:
+        "https://static.wixstatic.com/media/02a5b1_b424626a2d3d43f6a368b7ee301b01e5~mv2.webp",
+    },
+  ];
 
-    const speakerData = [
-        {
-            "name" : "Raymond Chin",
-            "title": "Founder Sevenpreneur",
-            "image" : "https://static.wixstatic.com/media/02a5b1_dad1b41047da42bd92dd2eba42418c37~mv2.webp",
-        },
-        {
-            "name" : "Basuki Tjahaja Purnama (Ahok)",
-            "title": "Former Governor of Jakarta 2014 - 2017",
-            "image" : "https://static.wixstatic.com/media/02a5b1_7933f447a8d443359f3c9d07c6a40a08~mv2.webp",
-        },
-        {
-            "name" : "Denny Santoso",
-            "title": "Founder & CEO BrainBoost",
-            "image" : "https://static.wixstatic.com/media/02a5b1_e9e8d63f9e6d423ea7dab593d6009207~mv2.webp",
-        },
-        {
-            "name" : "Irzan Raditya",
-            "title": "CEO & Co-Founder Kata.ai",
-            "image" : "https://static.wixstatic.com/media/02a5b1_2ecea6c5c5b34534bd47f20a62f2c7b8~mv2.webp",
-        },
-        {
-            "name" : "Bhima Yudhistira",
-            "title": "Executive Director CELIOS",
-            "image" : "https://static.wixstatic.com/media/02a5b1_a015ebc361e5456a9cc5c72f569106da~mv2.webp",
-        },
-        {
-            "name" : "And more speakers",
-            "title": "will be revealed",
-            "image" : "https://static.wixstatic.com/media/02a5b1_b424626a2d3d43f6a368b7ee301b01e5~mv2.webp",
-        },
-    ]
-
-    return(
-        <section id="speakers-lineup">
-        <div className="speakers-lineup pb-8 flex flex-col items-center gap-3 lg:gap-5 lg:pb-[60px]">
-            <div className="section-name flex flex-col items-center">
-                <SectionTitleRestart25 sectionTitle="Speakers Lineup"/>
-            </div>
-            <div className="flex flex-wrap max-w-[920px] items-start justify-center gap-4 lg:gap-6">
-                {speakerData.map((post, index) => (
-                    <SpeakerItemRestart25
-                    key={index}
-                    speakerName={post.name}
-                    speakerImage={post.image}
-                    speakerTitle={post.title}/>
-                ))}
-            </div>
+  return (
+    <section id="speakers-lineup">
+      <div className="speakers-lineup pb-8 flex flex-col items-center gap-3 lg:gap-5 lg:pb-[60px]">
+        <div className="section-name flex flex-col items-center">
+          <SectionTitleRestart25 sectionTitle="Speakers Lineup" />
         </div>
-        </section>
-    )
+        <div className="flex flex-wrap max-w-[920px] items-start justify-center gap-4 lg:gap-6">
+          {speakerData.map((post, index) => (
+            <SpeakerItemRestart25
+              key={index}
+              speakerName={post.name}
+              speakerImage={post.image}
+              speakerTitle={post.title}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
