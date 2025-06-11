97d46fadfa0fb42b5a1afb8a78d2b2b4ad220e9d
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-11 12:07:57 +0700

    adjust ahok position

--- src/app/components/templates/HeroEventRestart25.jsx
+++ src/app/components/templates/HeroEventRestart25.jsx
@@ -37,7 +37,7 @@ export default function HeroEventRestart2025() {
       {/* --- Brand Representative */}
       <Image
         className="brand-representative absolute flex max-w-[420px] top-[108px] left-1/2 -translate-x-1/2 z-30 lg:max-w-[860px] lg:top-[186px]"
-        src="https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur//brand-representative%20(1).webp"
+        src="https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur//brand-representative-7p.webp"
         alt="Sevenpreneur Brand Representative"
         width={2440}
         height={2440}
