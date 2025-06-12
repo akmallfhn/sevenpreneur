7c63251e535b8dedebe7483688c86bb085b6ebba
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-06-12 21:00:00 +0700

    Add list.* endpoints
    New endpoints:
    - list.industries
    - list.entrepreneurStages
    - list.roles
    - list.users
    
    Related: SVP-37

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "@/trpc/init";
 import { authRouter } from "./auth";
 import { helloRouter } from "./hello";
+import { listRouter } from "./list";
 
 export const appRouter = createTRPCRouter({
   hello: helloRouter,
   auth: authRouter,
+  list: listRouter,
 });
 
 export type AppRouter = typeof appRouter;
