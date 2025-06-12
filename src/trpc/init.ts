73935776b351dcef55661c2e9a5cd02bc4ceff8f
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-06-12 18:00:00 +0700

    [BE] Add loggedInProcedure using context and authorization header

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,12 +1,40 @@
 import { PrismaClient } from "@prisma/client";
-import { initTRPC } from "@trpc/server";
-import { cache } from "react";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { headers } from "next/headers";
 
 const prisma = new PrismaClient();
 
-export const createTRPCContext = cache(async () => {
-  return { prisma };
-});
+export async function createTRPCContext() {
+  async function getUserFromHeader() {
+    const heads = await headers();
+    const authHeader = heads.get("authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return null;
+    }
+
+    const sessionToken = authHeader.substring(7);
+    const tokenObj = await prisma.token.findUnique({
+      include: {
+        user: {
+          include: {
+            role: true,
+          },
+        },
+      },
+      where: {
+        token: sessionToken,
+      },
+    });
+    if (!tokenObj) {
+      return null;
+    }
+
+    return tokenObj.user;
+  }
+  const user = await getUserFromHeader();
+
+  return { prisma, user };
+}
 
 export type TRPCContext = Awaited<ReturnType<typeof createTRPCContext>>;
 
@@ -15,3 +43,16 @@ const t = initTRPC.context<TRPCContext>().create();
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const loggedInProcedure = t.procedure.use(async (opts) => {
+  const { ctx } = opts;
+  if (!ctx.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return opts.next({
+    ctx: {
+      prisma: ctx.prisma,
+      user: ctx.user, // not-null
+    },
+  });
+});
