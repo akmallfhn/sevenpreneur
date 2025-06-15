283f445b19fb36511d1f5fe37fb14fd9033c1330
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-15 19:52:03 +0700

    creating form submission

--- src/app/(admin)/admin/users/create/page.jsx
+++ src/app/(admin)/admin/users/create/page.jsx
@@ -11,34 +11,7 @@ export default async function CreateUserPage() {
 
   return (
     <div className="root flex w-full h-full bg-white justify-center bg-main-root py-8 overflow-y-auto lg:pl-64">
-      <div className="index-article w-[1040px] flex flex-col gap-4">
-        {/* --- PAGE HEADER */}
-        <div className="page-header flex flex-col gap-3">
-          <div className="page-title-actions flex justify-between items-center">
-            {/* --- Page Title */}
-            <TitleRevealCMS
-              titlePage={"Add New User"}
-              descPage={
-                "Manage your published content easily. Click on an article to view or edit its details."
-              }
-            />
-
-            {/* --- Page Actions */}
-            <div className="page-actions flex items-center gap-4">
-              <Link href={"/users"}>
-                <AppButton variant="outline">Cancel</AppButton>
-              </Link>
-              <AppButton variant="cmsPrimary">
-                <Save className="size-5" />
-                Add New User
-              </AppButton>
-            </div>
-          </div>
-        </div>
-
-        {/* --- CREATE USER FORM */}
-        <CreateUserForm sessionToken={sessionToken} />
-      </div>
+      <CreateUserForm sessionToken={sessionToken} />
     </div>
   );
 }
