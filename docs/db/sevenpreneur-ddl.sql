65c4fdb5e5825294e2954e8309e61d2132931448
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-06-16 07:00:00 +0700

    [DB] Add triggers for updated_at columns
    New function: update_updated_at()

--- docs/db/sevenpreneur-ddl.sql
+++ docs/db/sevenpreneur-ddl.sql
@@ -58,3 +58,29 @@ CREATE TABLE tokens (
   token       TEXT         NOT NULL  UNIQUE,
   created_at  TIMESTAMPTZ  NOT NULL  DEFAULT CURRENT_TIMESTAMP
 );
+
+---------------
+-- Functions --
+---------------
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+  RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.updated_at := CURRENT_TIMESTAMP;
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
+--------------
+-- Triggers --
+--------------
+
+CREATE TRIGGER update_roles_updated_at_trigger
+  BEFORE UPDATE ON roles
+  FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER update_users_updated_at_trigger
+  BEFORE UPDATE ON users
+  FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at();
