60ef38c95bc366c14b9e58ba4a34e43ee6ef851b
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-18 19:51:26 +0700

    undo Disable batching for tRPC calls

--- src/app/(api)/api/trpc/[trpc]/route.ts
+++ src/app/(api)/api/trpc/[trpc]/route.ts
@@ -69,7 +69,6 @@ const handler = (req: Request) => {
         },
       };
     },
-    allowBatching: false,
   });
 };
 
--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -2,7 +2,7 @@
 
 import type { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { httpLink } from "@trpc/client";
+import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
@@ -36,7 +36,7 @@ export function TRPCProvider(
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
-        httpLink({
+        httpBatchLink({
           url: props.baseURL,
           headers() {
             if (sessionToken.trim().length > 0) {
