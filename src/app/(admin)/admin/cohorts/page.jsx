36d9bcbb3604629525610aafd5093ee58370d2e1
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-19 16:44:05 +0700

    added cohort list page, global metadata, refactor title cms to tsx, improve desc page. Related: SVP-54

--- src/app/(admin)/admin/cohorts/page.jsx
+++ src/app/(admin)/admin/cohorts/page.jsx
@@ -1,7 +1,67 @@
-export default async function AdminHomePage() {
+import AppBreadcrumb from "@/app/components/elements/AppBreadcrumb";
+import AppBreadcrumbItem from "@/app/components/elements/AppBreadcrumbItem";
+import AppButton from "@/app/components/elements/AppButton";
+import { ChevronRight, PlusCircle } from "lucide-react";
+import TitleRevealCMS from "@/app/components/elements/TitleRevealCMS";
+import CohortItemCardCMS from "@/app/components/elements/CohortItemCardCMS";
+
+export default async function CohortListPage() {
   return (
-    <div className="pl-72">
-      <p>Welcome to Cohort page!</p>
+    <div className="root flex w-full h-full bg-white justify-center bg-main-root py-8 pb-24 overflow-y-auto lg:pl-64">
+      <div className="container max-w-[calc(100%-4rem)] w-full flex flex-col gap-4">
+        {/* --- PAGE HEADER */}
+        <div className="page-header flex flex-col gap-3">
+          <AppBreadcrumb>
+            <ChevronRight className="size-3.5" />
+            <AppBreadcrumbItem href="/cohorts" isCurrentPage>
+              Cohorts
+            </AppBreadcrumbItem>
+          </AppBreadcrumb>
+          <div className="page-title-actions flex justify-between items-center">
+            {/* --- Page Title */}
+            <TitleRevealCMS
+              titlePage={"Cohort Event"}
+              descPage={
+                "Manage your published content easily. Click on an article to view or edit its details."
+              }
+            />
+
+            {/* --- Page Actions */}
+            <AppButton variant="cmsPrimary">
+              <PlusCircle className="size-5" />
+              Create Cohort
+            </AppButton>
+          </div>
+        </div>
+
+        {/* --- List Cohort */}
+        <div className="w-full flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <h2 className="font-bold font-brand text-xl text-black">
+              Active Event
+            </h2>
+            <div className="flex flex-wrap gap-4">
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="font-bold font-brand text-xl text-black">
+              Finished Event
+            </h2>
+            <div className="flex flex-wrap gap-4">
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+              <CohortItemCardCMS />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/components/elements/CohortItemCardCMS.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import { faCalendar, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import StatusLabelCMS from "./StatusLabelCMS";
+import AppButton from "./AppButton";
+import { EllipsisVertical } from "lucide-react";
+
+export default function CohortItemCardCMS() {
+  return (
+    <div className="based flex flex-col max-w-[252px] bg-white border border-outline rounded-md overflow-hidden">
+      {/* --- Thumbnail */}
+      <div className="image-thumbnail relative aspect-thumbnail overflow-hidden">
+        <Image
+          className="object-cover w-full h-full"
+          src={
+            "https://static.wixstatic.com/media/02a5b1_75a55654d4b445da8c4500b84f0cb7a2~mv2.webp"
+          }
+          alt="thumbnail"
+          width={500}
+          height={500}
+        />
+        <div className="absolute top-2 left-2">
+          <StatusLabelCMS labelName="ON SCHEDULE" variants="ACTIVE" />
+        </div>
+      </div>
+      {/* --- Metadata */}
+      <div className="metadata relative flex flex-col p-3 gap-2">
+        <h3 className="cohort-title text-lg font-bodycopy font-bold line-clamp-2">
+          Kelas Memancing Ikan Pada Hari Jumat Sabtu Minggu Negara
+        </h3>
+        <div className="cohort-participant flex gap-2 items-center text-alternative">
+          <FontAwesomeIcon icon={faUser} className="size-3" />
+          <p className="font-bodycopy font-medium text-sm">
+            1,934 people joined
+          </p>
+        </div>
+        <div className="cohort-timeline flex gap-2 items-center text-alternative">
+          <FontAwesomeIcon icon={faCalendar} className="size-3  " />
+          <p className="font-bodycopy font-medium text-sm">
+            15 Jun - 16 Jun 2025
+          </p>
+        </div>
+        <div className="absolute bottom-1 right-0">
+          <AppButton variant="ghost" size="small">
+            <EllipsisVertical className="size-4" />
+          </AppButton>
+        </div>
+      </div>
+    </div>
+  );
+}
