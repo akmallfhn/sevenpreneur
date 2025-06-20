4ca4f679b896fa06ebf59237092faa7d43aa2917
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-20 17:05:13 +0700

    fix custom floating button

--- src/app/components/elements/CustomFloatingRestart25.jsx
+++ src/app/components/elements/CustomFloatingRestart25.jsx
@@ -19,8 +19,8 @@ export default function CustomFloatingRestart25() {
     <div
       className={`fixed max-w-[160px] w-full bottom-16 right-5 z-[80] transition transform ease-in-out active:scale-95 md:max-w-[220px] md:bottom-7 md:right-7 ${
         showButton
-          ? "md:opacity-100 md:pointer-events-auto md:translate-x-0 md:duration-500"
-          : "md:opacity-0 md:pointer-events-none md:translate-x-full md:duration-300"
+          ? "opacity-100 pointer-events-auto translate-x-0 duration-500"
+          : "opacity-0 pointer-events-none translate-x-full duration-300"
       }`}
     >
       <Link href={"#ticket-id"}>
