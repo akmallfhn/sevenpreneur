e3ee07af6046e70334bb0a6c8e12e1cf22192ed1
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-22 11:46:40 +0700

    refactor ssr to tsx

--- src/app/(admin)/admin/users/create/page.tsx
+++ src/app/(admin)/admin/users/create/page.tsx
@@ -3,7 +3,7 @@ import CreateUserForm from "@/app/components/forms/CreateUserFormCMS";
 
 export default async function CreateUserPage() {
   const cookieStore = await cookies();
-  const sessionToken = cookieStore.get("session_token")?.value;
+  const sessionToken = cookieStore.get("session_token")?.value ?? "";
 
   return (
     <div className="root flex w-full h-full bg-white justify-center bg-main-root py-8 overflow-y-auto lg:pl-64">
---
+++ src/app/(admin)/admin/users/page.tsx
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import UserListCMS from "@/app/components/templates/UserListCMS";
+
+export default async function UserListPage() {
+  const cookieStore = await cookies();
+  const sessionToken = cookieStore.get("session_token")?.value ?? "";
+
+  return (
+    <div className="root flex w-full h-full bg-white justify-center bg-main-root py-8 pb-24 overflow-y-auto lg:pl-64">
+      <UserListCMS sessionToken={sessionToken} />
+    </div>
+  );
+}
