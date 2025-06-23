0a5cd34fc1554cede01d0d5d8a9cfb2bb8084729
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-06-23 21:07:46 +0700

    integrate cohort list, change logo floating button, revise business challenge finale

--- src/app/(admin)/admin/cohorts/page.tsx
+++ src/app/(admin)/admin/cohorts/page.tsx
@@ -1,9 +1,13 @@
 import CohortListCMS from "@/app/components/templates/CohortListCMS";
+import { cookies } from "next/headers";
 
 export default async function CohortListPage() {
+  const cookieStore = await cookies();
+  const sessionToken = cookieStore.get("session_token")?.value ?? "";
+
   return (
     <div className="root flex w-full h-full bg-white justify-center bg-main-root py-8 pb-24 overflow-y-auto lg:pl-64">
-      <CohortListCMS />
+      <CohortListCMS sessionToken={sessionToken} />
     </div>
   );
 }
