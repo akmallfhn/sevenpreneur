94efb09349df89c932693310927f294701cacf4d
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-06-25 21:00:00 +0700

    [Infra] Use next.config.mjs to handle subdomain
    Related: SVP-89

--- next.config.mjs
+++ next.config.mjs
@@ -25,12 +25,102 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "header",
+            key: "host",
+            value: "(agora|admin).sevenpreneur.com.*",
+          },
+        ],
+        missing: [
+          {
+            type: "cookie",
+            key: "session_token",
+          },
+        ],
+        destination: "https://www.sevenpreneur.com/auth/login",
+        basePath: false,
+        permanent: false,
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "header",
+            key: "host",
+            value: "(agora|admin).example.com:3000.*",
+          },
+        ],
+        missing: [
+          {
+            type: "cookie",
+            key: "session_token",
+          },
+        ],
+        destination: "https://www.example.com:3000/auth/login",
+        basePath: false,
+        permanent: false,
+      },
+      {
+        source: "/auth(.*)",
+        has: [
+          {
+            type: "header",
+            key: "host",
+            value: "www.(sevenpreneur|example).com.*",
+          },
+          {
+            type: "cookie",
+            key: "session_token",
+            value: undefined,
+          },
+        ],
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/(admin|agora|api|www)",
+          destination: "/_not-found/page",
+        },
+      ],
+      afterFiles: [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "header",
+              key: "host",
+              value: "(?<subdomain>[^.]+).(sevenpreneur|example).com.*",
+            },
+          ],
+          destination: "/:subdomain/:path*",
+        },
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "header",
+              key: "host",
+              value: "(sevenpreneur|example).com.*",
+            },
+          ],
+          destination: "/www/:path*",
+        },
+      ],
+    };
+  },
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        "www.sevenpreneur.com",
-        "www.example.com:3000",
-      ],
+      allowedOrigins: ["www.sevenpreneur.com", "www.example.com:3000"],
     },
   },
 };
