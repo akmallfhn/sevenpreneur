726112db05eb618f259b5ee597ee0d6d26af1d77
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-07-02 20:09:12 +0700

    added learning details CMS

--- src/app/(admin)/admin/cohorts/[cohort_id]/learnings/[learning_id]/page.tsx
+++ src/app/(admin)/admin/cohorts/[cohort_id]/learnings/[learning_id]/page.tsx
@@ -1,27 +1,29 @@
 import CohortDetailsCMS from "@/app/components/templates/CohortDetailsCMS";
+import LearningDetailsCMS from "@/app/components/templates/LearningDetailsCMS";
 import { cookies } from "next/headers";
 
 interface LearningDetailPageProps {
-  params: Promise<{ cohort_id: string }>;
+  params: Promise<{ cohort_id: string; learning_id: string }>;
 }
 
 export default async function LearningDetailPage({
   params,
 }: LearningDetailPageProps) {
-  const { cohort_id } = await params;
+  const { cohort_id, learning_id } = await params;
   const cookieStore = await cookies();
   const sessionToken = cookieStore.get("session_token")?.value ?? "";
   const cohortId = parseInt(cohort_id);
+  const learningId = parseInt(learning_id);
+
+  console.log("learningId:", learningId);
 
   return (
     <div className="root hidden w-full h-full justify-center bg-white py-8 lg:flex lg:pl-64">
-      <p>Metadata sesi: title | description</p>
-      <p>Upload video recording</p>
-      <p>Edit Metode Meeting : online | offline | on site </p>
-      <p>Meeting: url | date-time</p>
-      <p>assign speaker</p>
-      <p>nambahin dokumen materi</p>
-      <p>future: nambahin quiz</p>
+      <LearningDetailsCMS
+        sessionToken={sessionToken}
+        cohortId={cohortId}
+        learningId={learningId}
+      />
     </div>
   );
 }
