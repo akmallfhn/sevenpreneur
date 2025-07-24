e26f88d8a0c5415aa4e257c72c6cfc408d30796b
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-07-24 23:00:56 +0700

    [FE] Create Landing Page SBBP Batch 7

---
+++ src/app/components/pages/CohortSBBPBatch7SVP.tsx
@@ -0,0 +1,12 @@
+"use client";
+import HeroTeaserRestart25 from "../templates/HeroTeaserRestart25";
+import OfferHighlightCardSVP from "../templates/OfferHighlightCardSVP";
+
+export default function CohortSBBPBatch7SVP() {
+  return (
+    <div className="root relative items-center">
+      <HeroTeaserRestart25 />
+      <OfferHighlightCardSVP />
+    </div>
+  );
+}
