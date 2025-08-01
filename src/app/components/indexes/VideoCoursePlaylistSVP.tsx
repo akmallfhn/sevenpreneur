e59b01bac7a78ac828b4a43eeaa3570e44dfee1c
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-08-01 21:25:28 +0700

    [FE] Add Metadata, Auto Correction Slug URL, Add Floating CTA

--- src/app/components/indexes/VideoCoursePlaylistSVP.tsx
+++ src/app/components/indexes/VideoCoursePlaylistSVP.tsx
@@ -23,6 +23,7 @@ export default function VideoCoursePlaylistSVP({
           key={index}
           index={index + 1}
           videoName={post.name}
+          videoImage={post.image_url}
         />
       ))}
     </div>
