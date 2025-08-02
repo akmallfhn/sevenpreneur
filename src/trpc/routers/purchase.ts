5bf3cce2a95c600247d7aa4eb3d732987530358a
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-08-02 16:00:00 +0700

    [BE] Refactor transaction operation to support other categories

--- src/trpc/routers/purchase.ts
+++ src/trpc/routers/purchase.ts
@@ -10,9 +10,140 @@ import {
   stringIsNanoid,
   stringNotBlank,
 } from "@/trpc/utils/validation";
-import { CategoryEnum, TStatusEnum } from "@prisma/client";
+import { CategoryEnum, PrismaClient, TStatusEnum } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+
+async function createTransaction(
+  prisma: PrismaClient,
+  userId: string,
+  category: CategoryEnum,
+  item: {
+    id: number;
+    name: string;
+    amount: Decimal;
+  },
+  paymentChannelId: number
+): Promise<{ transactionId: string; invoiceUrl: string }> {
+  const selectedPayment = await prisma.paymentChannel.findFirst({
+    where: { id: paymentChannelId },
+  });
+  if (!selectedPayment) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: "The payment channel with the given ID is not found.",
+    });
+  }
+  const calculatedPrice = calculateFinalPrice(item.amount, selectedPayment);
+  const createdTransaction = await prisma.transaction.create({
+    data: {
+      user_id: userId,
+      category: category,
+      item_id: item.id,
+      amount: item.amount,
+      admin_fee: calculatedPrice.adminFee,
+      vat: calculatedPrice.vat,
+      currency: "IDR",
+      invoice_number: "?", // updated after requesting Xendit
+      status: TStatusEnum.PENDING,
+      payment_method: selectedPayment.method,
+      payment_channel: selectedPayment.code,
+    },
+  });
+  const theTransaction = await prisma.transaction.findFirst({
+    where: { id: createdTransaction.id },
+  });
+  if (!theTransaction) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to create a new transaction.",
+    });
+  }
+
+  let domain = "sevenpreneur.com";
+  if (process.env.DOMAIN_MODE === "local") {
+    domain = "example.com:3000";
+  }
+  let xenditResponse: XenditInvoiceResponse;
+  try {
+    xenditResponse = await xenditRequestCreateInvoice({
+      external_id: theTransaction.id,
+      amount: calculatedPrice.finalPrice.toNumber(),
+      description: item.name,
+      invoice_duration: 12 * 60 * 60, // 12 hours
+      success_redirect_url: `https://www.${domain}/transactions/${theTransaction.id}`,
+      failure_redirect_url: `https://www.${domain}/transactions/${theTransaction.id}`,
+      payment_methods: [selectedPayment.code],
+      currency: theTransaction.currency,
+      items: [
+        {
+          name: item.name,
+          quantity: 1,
+          price: calculatedPrice.finalPrice.toNumber(),
+        },
+      ],
+    });
+  } catch (e) {
+    // Undo transaction creation
+    const deletedTransaction = await prisma.transaction.deleteMany({
+      where: { id: theTransaction.id },
+    });
+    if (deletedTransaction.count > 1) {
+      console.error(
+        "purchase: More-than-one transactions are deleted at once."
+      );
+    }
+    // Rethrow error using TRPCError
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to create a new invoice.",
+    });
+  }
+
+  const updatedTransaction = await prisma.transaction.updateManyAndReturn({
+    data: {
+      invoice_number: xenditResponse.id,
+    },
+    where: { id: theTransaction.id },
+  });
+  if (updatedTransaction.length < 1) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: "The selected transaction is not found.",
+    });
+  } else if (updatedTransaction.length > 1) {
+    console.error("purchase: More-than-one transactions are updated at once.");
+  }
+
+  return {
+    transactionId: theTransaction.id,
+    invoiceUrl: xenditResponse.invoice_url,
+  };
+}
+
+async function changePhoneNumber(
+  prisma: PrismaClient,
+  userId: string,
+  phoneCountryId: number,
+  phoneNumber: string
+) {
+  const updatedUser = await prisma.user.updateManyAndReturn({
+    data: {
+      phone_country_id: phoneCountryId,
+      phone_number: phoneNumber,
+    },
+    where: { id: userId },
+  });
+  if (updatedUser.length < 1) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: "The selected user is not found.",
+    });
+  } else if (updatedUser.length > 1) {
+    console.error("purchase: More-than-one users are updated at once.");
+  }
+}
 
 export const purchaseRouter = createTRPCRouter({
   cohort: loggedInProcedure
@@ -37,129 +168,33 @@ export const purchaseRouter = createTRPCRouter({
           message: "The cohort price with the given ID is not found.",
         });
       }
-      const selectedPayment = await opts.ctx.prisma.paymentChannel.findFirst({
-        where: { id: opts.input.payment_channel_id },
-      });
-      if (!selectedPayment) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "The payment channel with the given ID is not found.",
-        });
-      }
-      const calculatedPrice = calculateFinalPrice(
-        selectedCohortPrice.amount,
-        selectedPayment
-      );
-      const createdTransaction = await opts.ctx.prisma.transaction.create({
-        data: {
-          user_id: opts.ctx.user.id,
-          category: CategoryEnum.COHORT,
-          item_id: selectedCohortPrice.id,
+
+      const { transactionId, invoiceUrl } = await createTransaction(
+        opts.ctx.prisma,
+        opts.ctx.user.id,
+        CategoryEnum.COHORT,
+        {
+          id: selectedCohortPrice.id,
+          name: `${selectedCohortPrice.cohort.name} (${selectedCohortPrice.name})`,
           amount: selectedCohortPrice.amount,
-          admin_fee: calculatedPrice.adminFee,
-          vat: calculatedPrice.vat,
-          currency: "IDR",
-          invoice_number: "?", // updated after requesting Xendit
-          status: TStatusEnum.PENDING,
-          payment_method: selectedPayment.method,
-          payment_channel: selectedPayment.code,
         },
-      });
-      const theTransaction = await opts.ctx.prisma.transaction.findFirst({
-        where: { id: createdTransaction.id },
-      });
-      if (!theTransaction) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to create a new transaction.",
-        });
-      }
-
-      let domain = "sevenpreneur.com";
-      if (process.env.DOMAIN_MODE === "local") {
-        domain = "example.com:3000";
-      }
-      let xenditResponse: XenditInvoiceResponse;
-      try {
-        xenditResponse = await xenditRequestCreateInvoice({
-          external_id: theTransaction.id,
-          amount: calculatedPrice.finalPrice.toNumber(),
-          description: selectedCohortPrice.cohort.name,
-          invoice_duration: 12 * 60 * 60, // 12 hours
-          success_redirect_url: `https://www.${domain}/transactions/${theTransaction.id}`,
-          failure_redirect_url: `https://www.${domain}/transactions/${theTransaction.id}`,
-          payment_methods: [selectedPayment.code],
-          currency: theTransaction.currency,
-          items: [
-            {
-              name: selectedCohortPrice.cohort.name,
-              quantity: 1,
-              price: calculatedPrice.finalPrice.toNumber(),
-            },
-          ],
-        });
-      } catch (e) {
-        // Undo transaction creation
-        const deletedTransaction = await opts.ctx.prisma.transaction.deleteMany(
-          {
-            where: { id: theTransaction.id },
-          }
-        );
-        if (deletedTransaction.count > 1) {
-          console.error(
-            "purchase.cohort: More-than-one transactions are deleted at once."
-          );
-        }
-        // Rethrow error using TRPCError
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to create a new invoice.",
-        });
-      }
-
-      const updatedTransaction =
-        await opts.ctx.prisma.transaction.updateManyAndReturn({
-          data: {
-            invoice_number: xenditResponse.id,
-          },
-          where: { id: theTransaction.id },
-        });
-      if (updatedTransaction.length < 1) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "The selected transaction is not found.",
-        });
-      } else if (updatedTransaction.length > 1) {
-        console.error(
-          "purchase.cohort: More-than-one transactions are updated at once."
-        );
-      }
+        opts.input.payment_channel_id
+      );
 
       if (opts.input.phone_country_id && opts.input.phone_number) {
-        const updatedUser = await opts.ctx.prisma.user.updateManyAndReturn({
-          data: {
-            phone_country_id: opts.input.phone_country_id,
-            phone_number: opts.input.phone_number,
-          },
-          where: { id: opts.ctx.user.id },
-        });
-        if (updatedUser.length < 1) {
-          throw new TRPCError({
-            code: "NOT_FOUND",
-            message: "The selected user is not found.",
-          });
-        } else if (updatedUser.length > 1) {
-          console.error(
-            "purchase.cohort: More-than-one users are updated at once."
-          );
-        }
+        await changePhoneNumber(
+          opts.ctx.prisma,
+          opts.ctx.user.id,
+          opts.input.phone_country_id,
+          opts.input.phone_number
+        );
       }
 
       return {
         status: 200,
         message: "Success",
-        transaction_id: theTransaction.id,
-        invoice_url: xenditResponse.invoice_url,
+        transaction_id: transactionId,
+        invoice_url: invoiceUrl,
       };
     }),
 
