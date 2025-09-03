90522d9bc6270d9793fcef714b40d9b5e8aba11d
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-09-03 19:29:37 +0700

    [FE] Rename File

--- src/app/components/custom-pages/BlueprintProgramBatch7SVP.tsx
+++ src/app/components/custom-pages/BlueprintProgramBatch7SVP.tsx
@@ -1,13 +1,10 @@
 "use client";
-import HeroCohortSBBPBatch7SVP from "../templates/HeroCohortSBBPBatch7SVP";
-import HeroTeaserRestart25 from "../templates/HeroTeaserRestart25";
-import OfferHighlightCardSVP from "../templates/OfferHighlightCardSVP";
+import HeroBlueprintProgramBatch7SVP from "../templates/HeroBlueprintProgramBatch7SVP";
 
 export default function BlueprintProgramBatch7SVP() {
   return (
     <div className="root relative items-center">
-      <HeroCohortSBBPBatch7SVP />
-      {/* <OfferHighlightCardSVP /> */}
+      {/* <HeroBlueprintProgramBatch7SVP /> */}
     </div>
   );
 }
