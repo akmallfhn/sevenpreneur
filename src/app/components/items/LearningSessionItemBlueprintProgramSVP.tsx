f18b5f61f0dc5690a3e1dcd9b1310e5c2a04b175
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-09-06 20:42:24 +0700

    [FE] Added Curiculum & Speakers

---
+++ src/app/components/items/LearningSessionItemBlueprintProgramSVP.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import Image from "next/image";
+
+export type LearningSessionVariantBlueprintProgramSVP =
+  | "frameworkPrimary"
+  | "frameworkSecondary"
+  | "founderSeries"
+  | "extraordinary";
+
+const variantStyles: Record<
+  LearningSessionVariantBlueprintProgramSVP,
+  {
+    background_color: string;
+    overlay_color: string;
+    asset_decoration: string;
+    image_decoration: string;
+  }
+> = {
+  frameworkPrimary: {
+    background_color: "bg-[#003399]",
+    overlay_color: "bg-[#E74D79]",
+    asset_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/asset-square-primary.svg",
+    image_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/bg-decoration-primary.svg",
+  },
+  frameworkSecondary: {
+    background_color: "bg-[#0165F6]",
+    overlay_color: "bg-[#003399]",
+    asset_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/asset-square-secondary-extra.svg",
+    image_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/bg-decoration-secondary.svg",
+  },
+  founderSeries: {
+    background_color: "bg-[#E74D79]",
+    overlay_color: "bg-[#B10032]",
+    asset_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/asset-square-fsos.svg",
+    image_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/bg-decoration-fsos.svg",
+  },
+  extraordinary: {
+    background_color: "bg-black",
+    overlay_color: "bg-[#3E3E3E]",
+    asset_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/asset-square-secondary-extra.svg",
+    image_decoration:
+      "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/bg-decoration-extra.svg",
+  },
+};
+
+interface LearningSessionItemBlueprintProgramSVPProps {
+  sessionNumber: string;
+  sessionName: string;
+  sessionDescription?: string;
+  sessionEducator: string;
+  sessionEducatorTitle: string;
+  sessionEducatorAvatar: string;
+  variant: LearningSessionVariantBlueprintProgramSVP;
+}
+
+export default function LearningSessionItemBlueprintProgramSVP({
+  sessionNumber,
+  sessionName,
+  sessionDescription,
+  sessionEducator,
+  sessionEducatorTitle,
+  sessionEducatorAvatar,
+  variant,
+}: LearningSessionItemBlueprintProgramSVPProps) {
+  const {
+    background_color,
+    overlay_color,
+    asset_decoration,
+    image_decoration,
+  } = variantStyles[variant];
+  const backgroundColor = background_color;
+  const overlayColor = overlay_color;
+  const assetDecoration = asset_decoration;
+  const imageDecoration = image_decoration;
+
+  return (
+    <div className="session-outline p-[1px] bg-gradient-to-br from-0% from-[#C4C4C4] to-65% to-[#30266D] rounded-md lg:rounded-lg">
+      <div
+        className={`session-item-container relative flex flex-col w-[340px] h-full text-white p-5 justify-between aspect-[340/170] lg:aspect-[624/302] lg:w-[594px] ${backgroundColor} rounded-md overflow-hidden lg:rounded-lg`}
+      >
+        <div className="flex flex-col z-40">
+          {sessionNumber && (
+            <h4 className="session-chapter font-bold font-brand text-2xl lg:text-[48px]">
+              {sessionNumber}
+            </h4>
+          )}
+          {!sessionNumber && (
+            <Image
+              className="flex max-w-[52px] pb-1 opacity-70 lg:max-w-[92px] lg:pb-3"
+              src={
+                "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/founder-series-logo.svg"
+              }
+              alt="Founder Series Sevenpreneur"
+              width={300}
+              height={300}
+            />
+          )}
+          <h3 className="session-title font-bold font-brand max-w-[184px] leading-tight text-transparent bg-clip-text bg-gradient-to-b from-0% from-[#D1CDCD] via-50% via-white to-100% to-[#D1CDCD] line-clamp-3 lg:text-2xl lg:max-w-[336px]">
+            {sessionName}
+          </h3>
+          {sessionDescription && (
+            <p className="session-desc font-bodycopy text-[10px] max-w-[184px] lg:text-lg lg:max-w-[332px]">
+              {sessionDescription}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col font-bodycopy z-40">
+          <h4 className="session-educator font-bold text-[10px] lg:text-lg">
+            {sessionEducator}
+          </h4>
+          <p className="session-educator-title text-[8px] lg:text-base">
+            {sessionEducatorTitle}
+          </p>
+        </div>
+
+        {/* Absolute Overlay */}
+        <Image
+          className="neon-side-top-left absolute flex w-44 top-0 left-0 opacity-50 lg:w-[227px]"
+          src={
+            "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur//effect-side-neon.webp"
+          }
+          alt="Effect Neon Side"
+          width={200}
+          height={200}
+          layout="raw"
+        />
+        <div
+          className={`circle-blur-center absolute flex ${overlayColor} w-[354px] h-[128px] -bottom-[115px] left-1/2 -translate-x-1/2 blur-[50px] rounded-full lg:w-[648px] lg:h-[235px] lg:-bottom-[216px]`}
+        />
+        <Image
+          className="asset-decoration absolute flex max-w-8 top-5 right-28 z-10 lg:max-w-14 lg:right-64"
+          src={assetDecoration}
+          alt="Asset Decoration"
+          width={200}
+          height={200}
+        />
+        <Image
+          className={`asset-decoration absolute flex w-full top-1/4 -translate-y-1/4 z-10 ${
+            variant === "frameworkSecondary" || "extraordinary"
+              ? "right-0"
+              : "right-2 lg:right-6"
+          } ${
+            variant === "extraordinary"
+              ? "max-w-[160px] lg:max-w-[300px]"
+              : "max-w-[120px] lg:max-w-[220px]"
+          }`}
+          src={imageDecoration}
+          alt="Image Decoration"
+          width={200}
+          height={200}
+        />
+        {/* Speakers Image */}
+        <div className="session-speaker-image absolute flex aspect-square w-[160px] bottom-0 -right-4 z-30 lg:w-[272px]">
+          <Image
+            className="object-cover w-full h-full"
+            src={sessionEducatorAvatar}
+            alt={sessionEducator}
+            width={400}
+            height={400}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/templates/CuriculumSessionsBlueprintProgramSVP.tsx
@@ -0,0 +1,198 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import LearningSessionItemBlueprintProgramSVP, {
+  LearningSessionVariantBlueprintProgramSVP,
+} from "../items/LearningSessionItemBlueprintProgramSVP";
+
+export default function CuriculumSessionsBlueprintProgramSVP() {
+  const data = [
+    {
+      session_number: "01.",
+      session_name: "Founder Mindset",
+      session_description:
+        "Before building a business, build the mindset that keeps it alive.",
+      session_educator: "Raymond Chin",
+      session_educator_title: "Founder Sevenpreneur",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkPrimary",
+    },
+    {
+      session_number: "02.",
+      session_name: "Business Ideation",
+      session_description:
+        "Great businesses don’t start with luck, they start with structured ideas.",
+      session_educator: "Vicktor Aritonang",
+      session_educator_title: "CEO SSPACE Media",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkSecondary",
+    },
+    {
+      session_number: "",
+      session_name: "From Personal Readiness to Strong Business Ideas",
+      session_description: "",
+      session_educator: "Zaky Muhammad Syah",
+      session_educator_title: "CEO Dibimbing & Cakrawala University",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "founderSeries",
+    },
+    {
+      session_number: "03.",
+      session_name: "Research Validation",
+      session_description:
+        "Start validating, so the idea stands on real ground.",
+      session_educator: "Yusuf Arezany",
+      session_educator_title: "Researcher Govtech Procurement",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkPrimary",
+    },
+    {
+      session_number: "",
+      session_name: "From Personal Readiness to Strong Business Ideas",
+      session_description: "",
+      session_educator: "Zaky Muhammad Syah",
+      session_educator_title: "CEO Dibimbing & Cakrawala University",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "founderSeries",
+    },
+    {
+      session_number: "04.",
+      session_name: "Sales & Marketing",
+      session_description:
+        "Master the art of attracting, converting, and retaining customers",
+      session_educator: "Vicktor Aritonang",
+      session_educator_title: "CEO SSPACE Media",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkSecondary",
+    },
+    {
+      session_number: "",
+      session_name: "Ways To Build Powerful Business Connections",
+      session_description: "",
+      session_educator: "Adythia Pratama",
+      session_educator_title: "Guerilla Marketing Specialist",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "founderSeries",
+    },
+    {
+      session_number: "05.",
+      session_name: "Product & Offer",
+      session_description:
+        "Turn what you have into what people can’t wait to pay for.",
+      session_educator: "Vander Lesnussa",
+      session_educator_title: "Senior Product Manager Allofresh",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkPrimary",
+    },
+    {
+      session_number: "06.",
+      session_name: "Operations",
+      session_description:
+        "Build the systems that keep your business running smoothly as it scales.",
+      session_educator: "Berry Boen",
+      session_educator_title: "COO Datascript",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkSecondary",
+    },
+    {
+      session_number: "07.",
+      session_name: "Growth and Beyond",
+      session_description:
+        "Scaling isn’t just bigger. It’s smarter, faster, and sustainable.",
+      session_educator: "Raymond Chin",
+      session_educator_title: "Founder Sevenpreneur",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "frameworkPrimary",
+    },
+    {
+      session_number: "07+",
+      session_name: "From Pitch Perfect to Investable Opportunity",
+      session_description: "",
+      session_educator: "Wafa Taftazani",
+      session_educator_title: "Investor",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "extraordinary",
+    },
+    {
+      session_number: "",
+      session_name:
+        "The Secrets Investors for Businesses Worth Funding and Go Global",
+      session_description: "",
+      session_educator: "Tom Lembong",
+      session_educator_title: "ex Trade Minister & Banking Investor",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "extraordinary",
+    },
+    {
+      session_number: "",
+      session_name:
+        "Ethics & Effectiveness: Drivers of Businesses and Investment",
+      session_description: "",
+      session_educator: "Basuki T. Purnama",
+      session_educator_title: "Governor of DKI Jakarta 2017",
+      session_educator_avatar:
+        "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/sbbp-speakers-raymond.webp",
+      session_variant: "extraordinary",
+    },
+  ];
+  return (
+    <div className="root relative flex items-center justify-center bg-gradient-to-b from-0% from-black via-60% via-[#2C2196] to-100% to-[#3426AE]">
+      {/* Container */}
+      <div className="container flex flex-col w-full items-center p-5 pb-24 gap-8 z-10 lg:px-0 lg:gap-[64px] lg:py-[60px] lg:max-w-[988px] xl:max-w-[1208px] 2xl:max-w-[1300px] lg:flex-row lg:items-start">
+        {/* Section Title & Desc */}
+        <div className="section-title-desc flex flex-col w-full text-center items-center gap-3 lg:sticky lg:top-28 lg:text-left lg:items-start">
+          <h2 className="section-title text-transparent w-fit bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#B89FE0] font-brand font-bold text-2xl lg:text-4xl">
+            Kurikulum Praktis yang Sudah Teruji
+          </h2>
+          <p className="section-desc text-sm font-bodycopy text-white max-w-[326px] lg:text-xl lg:max-w-[572px]">
+            Jalur belajar terstruktur dengan bimbingan coach, business leader,
+            dan CEO untuk membangun bisnis lebih terarah.
+          </p>
+        </div>
+
+        {/* Session Item */}
+        <div className="sessions flex flex-col gap-4">
+          {data.map((post, index) => (
+            <LearningSessionItemBlueprintProgramSVP
+              key={index}
+              variant={
+                post.session_variant as LearningSessionVariantBlueprintProgramSVP
+              }
+              sessionNumber={post.session_number}
+              sessionName={post.session_name}
+              sessionDescription={post.session_description}
+              sessionEducator={post.session_educator}
+              sessionEducatorTitle={post.session_educator_title}
+              sessionEducatorAvatar={post.session_educator_avatar}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Background */}
+      <div className="overlay-background absolute inset-x-0 bottom-0">
+        <Image
+          className="w-full h-auto object-cover mix-blend-plus-lighter"
+          src={
+            "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur/overlay-curiculum%201.webp"
+          }
+          alt="Overlay Background"
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </div>
+  );
+}
