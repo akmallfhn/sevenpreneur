a828c51735d20bbe79d5e4f9c0587fd917384ae1
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-09-12 11:54:42 +0700

    [BE] Added Meta Pixel Purchase Tracking

--- src/app/(api)/api/xendit/webhook/route.ts
+++ src/app/(api)/api/xendit/webhook/route.ts
@@ -49,6 +49,32 @@ export async function POST(req: NextRequest) {
       });
     }
 
+    // await fetch(
+    //   `https://graph.facebook.com/v18.0/${process.env.META_PIXEL_ID}/events`,
+    //   {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       data: [
+    //         {
+    //           event_name: "Purchase",
+    //           event_time: Math.floor(Date.now() / 1000),
+    //           user_data: {
+    //             em: theTransaction.user_id,
+    //           },
+    //           custom_data: {
+    //             currency: "IDR",
+    //             value: theTransaction.amount,
+    //             content_ids: [theTransaction.item_id],
+    //             content_type: "product",
+    //           },
+    //         },
+    //       ],
+    //       access_token: process.env.META_ACCESS_TOKEN,
+    //     }),
+    //   }
+    // );
+
     if (theTransaction.category === CategoryEnum.COHORT) {
       const theCohortPrice = await prisma.cohortPrice.findFirst({
         where: { id: theTransaction.item_id },
