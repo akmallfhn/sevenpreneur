061b202a9f9c98d09a9cacc01bd05db4bbc81af0
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-09-22 19:17:11 +0700

    [FE] Change font size

--- src/app/components/templates/BatchScheduleBlueprintProgramSVP.tsx
+++ src/app/components/templates/BatchScheduleBlueprintProgramSVP.tsx
@@ -6,7 +6,7 @@ export default function BatchScheduleBlueprintProgramSVP() {
   return (
     <div className="section-root relative flex items-center justify-center bg-black">
       <div className="batch-outline p-[1px] w-full h-full max-w-[378px] m-5 my-10 bg-gradient-to-br from-0% from-[#575757] to-100% to-[#3C3C3C] rounded-md overflow-hidden lg:max-w-[988px] xl:max-w-[1208px] 2xl:max-w-[1300px]">
-        <div className="batch-container relative flex flex-col w-full h-full p-5 gap-8 bg-gradient-to-br from-0% from-[#19181C] to-100% to-[#2D2D2E] rounded-md lg:flex-row lg:items-center lg:justify-between lg:p-9">
+        <div className="batch-container relative flex flex-col w-full h-full p-5 gap-8 bg-gradient-to-br from-0% from-[#0B0B0B] to-100% to-[#2D2D2E] rounded-md lg:flex-row lg:items-center lg:justify-between lg:p-9">
           <h2 className="batch-title text-transparent w-fit bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#B89FE0] font-brand font-bold text-2xl z-10 sm:text-3xl xl:text-4xl">
             Program Schedule
             <br />
@@ -25,15 +25,15 @@ export default function BatchScheduleBlueprintProgramSVP() {
               </div>
               <div className="hidden border-b border-dashed border-outline lg:flex" />
               <div className="schedule flex font-bodycopy gap-4 text-white lg:text-black">
-                <div className="day-schedule flex flex-col gap-1 w-fit">
-                  <p className="font-bold">Days</p>
+                <div className="day-schedule flex flex-col gap-1 w-fit text-sm lg:text-base">
+                  <p className="font-bold">Day</p>
                   <div className="font-medium">
                     <p>Mon - Fri</p>
                     <p>Saturday</p>
                   </div>
                 </div>
-                <div className="time-schedule flex flex-col gap-1 w-fit">
-                  <p className="font-bold">Class Sessions</p>
+                <div className="time-schedule flex flex-col gap-1 w-fit text-sm lg:text-base">
+                  <p className="font-bold">Class Session</p>
                   <div className="font-medium">
                     <p>19.00 - 21.00</p>
                     <p>09.00 - 16.00</p>
@@ -53,13 +53,13 @@ export default function BatchScheduleBlueprintProgramSVP() {
               </div>
               <div className="hidden border-b border-dashed border-outline lg:flex" />
               <div className="schedule flex font-bodycopy gap-4 text-white lg:text-black">
-                <div className="day-schedule flex flex-col gap-1 w-fit">
+                <div className="day-schedule flex flex-col gap-1 w-fit text-sm lg:text-base">
                   <p className="font-bold">Days</p>
                   <div className="font-medium">
                     <p>Sunday</p>
                   </div>
                 </div>
-                <div className="time-schedule flex flex-col gap-1 w-fit">
+                <div className="time-schedule flex flex-col gap-1 w-fit text-sm lg:text-base">
                   <p className="font-bold">Class Sessions</p>
                   <div className="font-medium">
                     <p>15.00 - 22.00</p>
