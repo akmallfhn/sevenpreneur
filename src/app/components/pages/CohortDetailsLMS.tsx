74438d12a083f500adf08da6b01a98e7dedf73cc
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-10-20 05:10:03 +0700

    [FE] Added Cohort Header Banner

--- src/app/components/pages/CohortDetailsLMS.tsx
+++ src/app/components/pages/CohortDetailsLMS.tsx
@@ -8,6 +8,7 @@ import CohortDetailsTabsLMS, {
   ProjectList,
   UserList,
 } from "../tabs/CohortDetailsTabsLMS";
+import HeaderCohortDetailsLMS from "../navigations/HeaderCohortDetailsLMS";
 
 interface CohortDetailsLMSProps extends AvatarBadgeLMSProps {
   sessionUserId: string;
@@ -33,31 +34,13 @@ export default function CohortDetailsLMS({
   userList,
 }: CohortDetailsLMSProps) {
   return (
-    <div className="root-page hidden w-full h-full gap-4 items-center justify-center pb-8 lg:flex lg:flex-col lg:pl-64">
-      <HeaderNavbarLMS
-        headerTitle="Bootcamp Details"
-        headerDescription="View all bootcamps you’ve purchased and enrolled in."
-        sessionUserRole={sessionUserRole}
+    <div className="root-page hidden w-full h-full gap-7 items-center justify-center pb-8 lg:flex lg:flex-col lg:pl-64">
+      <HeaderCohortDetailsLMS
         sessionUserName={sessionUserName}
         sessionUserAvatar={sessionUserAvatar}
+        sessionUserRole={sessionUserRole}
       />
       <div className="body-cohort max-w-[calc(100%-4rem)] w-full flex flex-col gap-4 rounded-xl">
-        <div className="header-cohort relative flex w-full items-center aspect-leaderboard-banner rounded-lg overflow-hidden">
-          <div className="metadata-cohort flex flex-col max-w-[528px] pl-8 gap-4 z-10 ">
-            <h1 className="font-brand font-bold text-2xl text-white">
-              {cohortName}
-            </h1>
-          </div>
-          <div></div>
-          <Image
-            className="object-cover w-full h-full"
-            src={
-              "https://tskubmriuclmbcfmaiur.supabase.co/storage/v1/object/public/sevenpreneur//leaderboard-wallpaper.jpeg"
-            }
-            alt="Header Banner Cohort"
-            fill
-          />
-        </div>
         <div className="flex justify-between gap-4">
           <main className="main flex flex-col flex-[2.5] w-full">
             <CohortDetailsTabsLMS
