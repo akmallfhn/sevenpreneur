ec8c69d29acd4ef52a0fe8c30df35977e03d938c
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-11-02 00:00:00 +0700

    [BE] Add an AI tool to calculate market size
    New endpoints:
    - read.ai.marketSize
    - use.ai.marketSize
    
    Related: SVP-224

--- src/trpc/routers/ai_tool/read.ai_tool.ts
+++ src/trpc/routers/ai_tool/read.ai_tool.ts
@@ -2,7 +2,7 @@ import { STATUS_OK } from "@/lib/status_code";
 import { loggedInProcedure } from "@/trpc/init";
 import { readFailedNotFound } from "@/trpc/utils/errors";
 import { objectHasOnlyID } from "@/trpc/utils/validation";
-import { AIResultIdeaGeneration } from "./prompt.ai_tool";
+import { AIResultIdeaGeneration, AIResultMarketSize } from "./prompt.ai_tool";
 import { isEnrolledAITool } from "./util.ai_tool";
 
 export const readAIResult = {
@@ -39,4 +39,36 @@ export const readAIResult = {
         },
       };
     }),
+
+  marketSize: loggedInProcedure.input(objectHasOnlyID()).query(async (opts) => {
+    if (opts.ctx.user.role.name === "General User") {
+      await isEnrolledAITool(
+        opts.ctx.prisma,
+        opts.ctx.user.id,
+        "You're not allowed to use AI tools."
+      );
+    }
+
+    const theAIResult = await opts.ctx.prisma.aIResult.findFirst({
+      select: { name: true, result: true, created_at: true },
+      where: {
+        id: opts.input.id,
+        user_id: opts.ctx.user.id,
+        ai_tool_id: 2, // market-size
+      },
+    });
+    if (!theAIResult) {
+      throw readFailedNotFound("AI result (market size)");
+    }
+
+    return {
+      code: STATUS_OK,
+      message: "Success",
+      result: {
+        name: theAIResult.name,
+        result: theAIResult.result as AIResultMarketSize,
+        created_at: theAIResult.created_at,
+      },
+    };
+  }),
 };
--- src/trpc/routers/ai_tool/use.ai_tool.ts
+++ src/trpc/routers/ai_tool/use.ai_tool.ts
@@ -1,7 +1,12 @@
 import { STATUS_OK } from "@/lib/status_code";
 import { loggedInProcedure } from "@/trpc/init";
+import { stringNotBlank } from "@/trpc/utils/validation";
 import z from "zod";
-import { aiToolPrompts } from "./prompt.ai_tool";
+import {
+  AIMarketSize_CustomerType,
+  AIMarketSize_ProductType,
+  aiToolPrompts,
+} from "./prompt.ai_tool";
 import { AIGenerate, AISaveResult, isEnrolledAITool } from "./util.ai_tool";
 
 export const useAITool = {
@@ -28,6 +33,55 @@ export const useAITool = {
         opts.ctx.prisma,
         opts.ctx.user.id,
         1, // idea-gen
+        parsedResult.title,
+        parsedResult.response
+      );
+
+      return {
+        code: STATUS_OK,
+        message: "Success",
+        title: parsedResult.title,
+        result: parsedResult.response,
+      };
+    }),
+
+  marketSize: loggedInProcedure
+    .input(
+      z.object({
+        additional_persona: stringNotBlank().optional(),
+        product_name: stringNotBlank(),
+        description: stringNotBlank(),
+        product_type: z.enum(AIMarketSize_ProductType),
+        customer_type: z.enum(AIMarketSize_CustomerType),
+        company_operating_area: stringNotBlank(),
+        sales_channel: stringNotBlank(),
+      })
+    )
+    .query(async (opts) => {
+      if (opts.ctx.user.role.name === "General User") {
+        await isEnrolledAITool(
+          opts.ctx.prisma,
+          opts.ctx.user.id,
+          "You're not allowed to use AI tools."
+        );
+      }
+
+      const parsedResult = await AIGenerate(
+        aiToolPrompts.marketSize(
+          opts.input.additional_persona || "",
+          opts.input.product_name,
+          opts.input.description,
+          opts.input.product_type,
+          opts.input.customer_type,
+          opts.input.company_operating_area,
+          opts.input.sales_channel
+        )
+      );
+
+      await AISaveResult(
+        opts.ctx.prisma,
+        opts.ctx.user.id,
+        2, // market-size
         parsedResult.title,
         parsedResult.response
       );
