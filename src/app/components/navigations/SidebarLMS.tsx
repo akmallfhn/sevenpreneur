e2032a7ddae5cbfdd35c723b3b3d86c0b2cedfe3
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-11-14 18:18:24 +0700

    [FE] Refactor Sidebar LMS

--- src/app/components/navigations/SidebarLMS.tsx
+++ src/app/components/navigations/SidebarLMS.tsx
@@ -48,10 +48,10 @@ export default function SidebarLMS({ aiResultList }: SidebarLMSProps) {
   };
 
   return (
-    <div className="sidebar-lms-root hidden fixed flex-col gap-4 max-w-64 w-full h-full inset-y-0 left-0 items-center bg-[#FCFDFE] backdrop-blur-md z-50 overflow-y-auto dark:bg-surface-black lg:flex">
-      <div className="sidebar-lms-top flex flex-col gap-4 w-full h-full max-w-[224px] pt-5">
-        <div className="sidebar-main-menu sticky top-0 flex flex-col w-full gap-5 bg-[#FCFDFE] z-10">
-          <div className="sidebar-lms-logo flex w-full p-2 px-2.5">
+    <div className="sidebar-lms-root hidden fixed flex-col max-w-64 w-full h-full inset-y-0 left-0 items-center bg-[#FCFDFE] backdrop-blur-md z-50 dark:bg-surface-black lg:flex">
+      <div className="sidebar-lms-container relative flex flex-col w-full h-full">
+        <div className="sidebar-main-menu fixed top-0 inset-x-0 flex flex-col w-full p-3 pt-5 items-center gap-4 bg-[#FCFDFE] z-10">
+          <div className="sidebar-logo flex w-48 p-2 px-2.5">
             <Image
               className="object-cover w-full h-full"
               src={
@@ -62,7 +62,7 @@ export default function SidebarLMS({ aiResultList }: SidebarLMSProps) {
               height={400}
             />
           </div>
-          <div className="sidebar-lms-menu flex flex-col h-full gap-2">
+          <div className="sidebar-lms-menu flex flex-col w-full h-full gap-2">
             <SidebarMenuItemLMS
               menuTitle="Dashboard"
               url="/"
@@ -91,42 +91,44 @@ export default function SidebarLMS({ aiResultList }: SidebarLMSProps) {
             />
           </div>
         </div>
-        {aiResultList.length > 0 && (
-          <div className="sidebar-lms-ai-result flex flex-col w-full gap-4">
-            <hr />
-            <div className="flex flex-col gap-1">
-              <h2 className="m-2 mt-0 text-sm text-alternative font-bodycopy font-medium">
-                GENERATED RESULT
-              </h2>
-              <div className="sidebar-ai-result flex flex-col h-full gap-2">
-                {aiResultList.map((post) => (
-                  <SidebarAIResultItemLMS
-                    key={post.id}
-                    aiToolSlug={post.ai_tool_slug_url}
-                    aiResultId={post.id}
-                    aiResultName={post.name || "Agora AI"}
-                  />
-                ))}
+        <div className="sidebar-scroll-body w-full h-full mt-[325px] mb-[68px] px-3 overflow-y-auto">
+          {aiResultList.length > 0 && (
+            <div className="sidebar-lms-ai-result flex flex-col w-full gap-4">
+              <hr />
+              <div className="flex flex-col gap-1">
+                <h2 className="m-2 mt-0 text-sm text-alternative font-bodycopy font-medium">
+                  GENERATED RESULT
+                </h2>
+                <div className="sidebar-ai-result flex flex-col h-full gap-2">
+                  {aiResultList.map((post) => (
+                    <SidebarAIResultItemLMS
+                      key={post.id}
+                      aiToolSlug={post.ai_tool_slug_url}
+                      aiResultId={post.id}
+                      aiResultName={post.name || "Agora AI"}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
+          )}
+        </div>
+        <div className="sidebar-lms-bottom fixed flex bottom-0 mx-auto w-full px-3 py-4 bg-[#FCFDFE] z-10">
+          <div
+            className={`logout-button flex w-full items-center p-2 px-2.5 gap-4 text-[#BE0E22] text-sm font-brand font-medium overflow-hidden rounded-md transition transform hover:cursor-pointer hover:bg-semi-destructive active:bg-semi-destructive active:scale-95 ${
+              isLoadingButton ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            onClick={handleLogout}
+          >
+            <div className="flex size-5 items-center justify-center">
+              {isLoadingButton ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                <LogOut />
+              )}
+            </div>
+            Logout
           </div>
-        )}
-      </div>
-      <div className="sidebar-lms-bottom sticky bottom-0 flex flex-col max-w-[224px] w-full py-4 bg-[#FCFDFE] gap-4 z-10">
-        <div
-          className={`logout-button flex w-full items-center p-2 px-2.5 gap-4 text-[#BE0E22] text-sm font-brand font-medium overflow-hidden rounded-md transition transform hover:cursor-pointer hover:bg-semi-destructive active:bg-semi-destructive active:scale-95 ${
-            isLoadingButton ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          onClick={handleLogout}
-        >
-          <div className="flex size-5 items-center justify-center">
-            {isLoadingButton ? (
-              <Loader2 className="animate-spin" />
-            ) : (
-              <LogOut />
-            )}
-          </div>
-          Logout
         </div>
       </div>
     </div>
