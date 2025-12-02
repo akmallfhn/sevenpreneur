a34e7f83ca95d2d62b008fe42767af6a702deff8
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2025-12-02 10:00:00 +0700

    [BE] Refactor QStash callback handler

--- src/app/(api)/api/qstash/callback-redis/route.ts
+++ src/app/(api)/api/qstash/callback-redis/route.ts
@@ -1,46 +1,11 @@
 import { SetAIResultEphemeral } from "@/lib/redis";
-import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
+import QStashHandlerVerified from "../handler";
 
-async function handler(req: Request) {
-  // The response from QStash
-  const result = await req.json();
-  const sourceMessageId = result.sourceMessageId as string;
-
-  // The response from OpenAI API
-  const body = JSON.parse(atob(result.body));
-
-  // The reply/assistant message
-  const assistantContents = [];
-  for (const output of body.output) {
-    if (
-      output.type === "message" &&
-      output.status === "completed" &&
-      output.role === "assistant"
-    ) {
-      assistantContents.push(...output.content);
-    }
-  }
-  let assistantText = "";
-  for (const content of assistantContents) {
-    if (content.type === "output_text") {
-      assistantText += content.text;
-    }
-  }
-  const content = JSON.parse(assistantText);
-
-  const savedResult = await SetAIResultEphemeral(
-    sourceMessageId,
-    content.response
-  );
+export const POST = QStashHandlerVerified(async ({ messageId, content }) => {
+  const savedResult = await SetAIResultEphemeral(messageId, content.response);
   if (!savedResult) {
     console.error(
-      `qstash.callback-redis: The AI result (${sourceMessageId}) is not found.`
+      `qstash.callback-redis: The AI result (${messageId}) is not found.`
     );
   }
-
-  return Response.json({
-    received: true, // Tell QStash that the result has been received successfully
-  });
-}
-
-export const POST = verifySignatureAppRouter(handler);
+});
