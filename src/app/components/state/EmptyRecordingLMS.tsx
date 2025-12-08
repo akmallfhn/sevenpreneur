77bb41a850d24b615e60fa06d0dc57e2ab6f1223
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-12-08 20:07:19 +0700

    [FE] Add Materials and Video Recording LMS

--- src/app/components/state/EmptyRecordingLMS.tsx
+++ src/app/components/state/EmptyRecordingLMS.tsx
@@ -5,7 +5,7 @@ export default function EmptyRecordingLMS() {
   return (
     <div className="state-box flex flex-col w-full p-6 items-center lg:px-0 lg:pt-0 lg:justify-center">
       <div className="state-container flex flex-col gap-4 max-w-md text-center items-center">
-        <div className="state-illustration flex max-w-36 overflow-hidden">
+        <div className="state-illustration flex max-w-24 overflow-hidden lg:max-w-36 ">
           <Image
             className="object-cover w-full h-full"
             src={
@@ -17,10 +17,10 @@ export default function EmptyRecordingLMS() {
           />
         </div>
         <div className="state-captions flex flex-col gap-1 items-center">
-          <h2 className="state-title flex font-bold font-bodycopy text-center text-xl tracking-tight">
+          <h2 className="state-title flex font-bold font-bodycopy text-center text-base tracking-tight lg:text-xl">
             Oops, No Video Yet!
           </h2>
-          <p className="state-description font-bodycopy text-center font-medium text-alternative text-[15px]">
+          <p className="state-description font-bodycopy text-center font-medium text-alternative text-sm lg:text-[15px]">
             Don’t worry! The video will be uploaded shortly after the session
             ends. Stay tuned!
           </p>
