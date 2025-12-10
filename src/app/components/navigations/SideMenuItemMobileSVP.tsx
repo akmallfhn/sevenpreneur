131f81b6b4230e1763c0dd5bbd59bd6f72473853
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-12-10 18:08:05 +0700

    [FE] Add return on Side Menu Item

--- src/app/components/navigations/SideMenuItemMobileSVP.tsx
+++ src/app/components/navigations/SideMenuItemMobileSVP.tsx
@@ -12,17 +12,19 @@ export default function SideMenuItemMobileSVP(
   props: SideMenuItemMobileSVPProps
 ) {
   if (props.menuURL) {
-    <Link href={props.menuURL} onClick={props.onClick}>
-      <li
-        className={`font-bodycopy font-medium text-base ${
-          props.destructiveColor
-            ? "text-destructive"
-            : "text-[#111111] dark:text-white"
-        }`}
-      >
-        {props.menuName}
-      </li>
-    </Link>;
+    return (
+      <Link href={props.menuURL} onClick={props.onClick}>
+        <li
+          className={`font-bodycopy font-medium text-base ${
+            props.destructiveColor
+              ? "text-destructive"
+              : "text-[#111111] dark:text-white"
+          }`}
+        >
+          {props.menuName}
+        </li>
+      </Link>
+    );
   }
 
   return (
