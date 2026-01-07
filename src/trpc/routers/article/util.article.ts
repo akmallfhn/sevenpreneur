55649169759ad27e5fb52c578d22da3305697b28
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2026-01-07 09:00:00 +0700

    [BE] Add reading_time to read.article
    The default is 200 wpm for Indonesian language.

--- src/trpc/routers/article/util.article.ts
+++ src/trpc/routers/article/util.article.ts
@@ -13,3 +13,13 @@ export type ArticleBodyContent =
       image_desc: null;
       content: string;
     }[];
+
+export const estimateWordCount = (htmlString: string) =>
+  htmlString
+    .replace(/<[^>]*>/g, " ")
+    .trim()
+    .match(/\s+/g)?.length ?? 0 + 1;
+
+// The speed is in word per minute. The default is 200 wpm for Indonesian language.
+export const estimateReadingTime = (htmlString: string, speed: number = 200) =>
+  estimateWordCount(htmlString) / speed;
