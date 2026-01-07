2da0fbb0e6e7ab94638ce7adea6818c48f1979a1
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2026-01-07 10:00:00 +0700

    [BE] Add table_of_contents to read.article

--- src/trpc/routers/article/util.article.ts
+++ src/trpc/routers/article/util.article.ts
@@ -1,3 +1,5 @@
+import { createSlugFromTitle } from "@/trpc/utils/slug";
+
 export type ArticleBodyContent =
   | {
       index_order: number;
@@ -23,3 +25,35 @@ export const estimateWordCount = (htmlString: string) =>
 // The speed is in word per minute. The default is 200 wpm for Indonesian language.
 export const estimateReadingTime = (htmlString: string, speed: number = 200) =>
   estimateWordCount(htmlString) / speed;
+
+export function getTableOfContents(
+  title: string,
+  bodyContent: ArticleBodyContent
+) {
+  const toc = [{ level: 1, name: title, id: createSlugFromTitle(title) }];
+
+  for (const entry of bodyContent) {
+    if (entry.sub_heading) {
+      toc.push({
+        level: 2,
+        name: entry.sub_heading,
+        id: createSlugFromTitle(entry.sub_heading),
+      });
+    }
+
+    if (entry.content) {
+      const headingsMatcher =
+        /<h(\d+)(?:[^>]*?(?=id="([^"]*?)"))?[^>]*?>(.*?)<\/h\1>/gi;
+      const headingList = [...entry.content.matchAll(headingsMatcher)];
+      for (const heading of headingList) {
+        toc.push({
+          level: Number(heading[1]),
+          name: heading[3],
+          id: heading[2] ?? createSlugFromTitle(heading[3]),
+        });
+      }
+    }
+  }
+
+  return toc;
+}
