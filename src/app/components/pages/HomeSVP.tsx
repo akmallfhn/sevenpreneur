702681a4fd3da37acb7b1ca6b47c33f7549962d1
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2026-01-08 10:08:50 +0700

    [FE] Add SVG Logo Components, Add Logo Business ALumni

--- src/app/components/pages/HomeSVP.tsx
+++ src/app/components/pages/HomeSVP.tsx
@@ -1,11 +1,13 @@
 "use client";
 import HeroHomeSVP from "../heroes/HeroHomeSVP";
+import BusinessesAlumniSVP from "../static-sections/BusinessesAlumniSVP";
 import CoachEducatorListSVP from "../static-sections/CoachEducatorListSVP";
 
 export default function HomeSVP() {
   return (
     <div className="root relative items-center">
       <HeroHomeSVP />
+      <BusinessesAlumniSVP />
       <CoachEducatorListSVP />
     </div>
   );
