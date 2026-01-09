d1df2c321ddf4f3fb0febd42decc9097377d4cf7
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2026-01-09 14:18:47 +0700

    [FE] Add Core Product Sevenpreneur

--- src/app/components/pages/HomeSVP.tsx
+++ src/app/components/pages/HomeSVP.tsx
@@ -2,12 +2,14 @@
 import HeroHomeSVP from "../heroes/HeroHomeSVP";
 import BusinessesAlumniSVP from "../static-sections/BusinessesAlumniSVP";
 import CoachEducatorListSVP from "../static-sections/CoachEducatorListSVP";
+import ProductListSVP from "../static-sections/ProductListSVP";
 
 export default function HomeSVP() {
   return (
     <div className="root relative items-center">
       <HeroHomeSVP />
-      <BusinessesAlumniSVP />
+      <ProductListSVP />
+      {/* <BusinessesAlumniSVP /> */}
       <CoachEducatorListSVP />
     </div>
   );
