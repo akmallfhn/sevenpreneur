d7685fb05086d41ff85bc950f2a4516e42545640
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2026-02-06 10:30:00 +0700

    [BE] Change comments about validations to be in JSDoc format

--- src/trpc/utils/validation.ts
+++ src/trpc/utils/validation.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
+/**
+ * See https://github.com/colinhacks/zod/issues/63#issuecomment-1429974422
+ */
 export function stringNotBlank(): z.ZodString {
-  // https://github.com/colinhacks/zod/issues/63#issuecomment-1429974422
   return z.string().trim().min(1);
 }
 
@@ -9,49 +11,68 @@ export function stringIsTimestampTz(): z.ZodISODateTime {
   return z.iso.datetime({ offset: true, local: false });
 }
 
+/**
+ * UUID simple format (hexadecimal)
+ */
 export function stringIsUUID(): z.ZodString {
-  // UUID simple format (hexadecimal)
   return z.string().trim().min(32);
 }
 
+/**
+ * Nanoid format (21 characters)
+ */
 export function stringIsNanoid(): z.ZodString {
-  // Nanoid format (21 character)
   return z.string().trim().min(21);
 }
 
+/**
+ * Number should be an integer, 1 or bigger
+ */
 export function numberIsID(): z.ZodInt {
-  // Number should be an integer, 1 or bigger
   return z.int().min(1);
 }
 
+/**
+ * Number should be an integer, 0 or bigger
+ *
+ * Administrator role has ID 0.
+ */
 export function numberIsRoleID(): z.ZodInt {
-  // Number should be an integer, 0 or bigger
-  // Administrator role has ID 0
   return z.int().min(0);
 }
 
+/**
+ * Number should be an integer, 1 or bigger
+ */
 export function numberIsPosInt(): z.ZodInt {
-  // Number should be an integer, 1 or bigger
   return z.int().min(1);
 }
 
+/**
+ * Number should be greater than 0
+ */
 export function numberIsPositive(): z.ZodNumber {
-  // Number should be greater than 0
   return z.number().gt(0);
 }
 
+/**
+ * Object only has one property `id` which is an ID (number)
+ */
 export function objectHasOnlyID() {
-  // Object only has one property which is ID
   return z.object({ id: numberIsID() });
 }
 
+/**
+ * Object only has one property `id` which is a nano ID
+ */
 export function objectHasOnlyNanoid() {
-  // Object only has one property which is nano ID
   return z.object({ id: stringIsNanoid() });
 }
 
+/**
+ * Object only has one property `id` which is a UUID
+ */
 export function objectHasOnlyUUID() {
-  // Object only has one property which is UUID
   return z.object({ id: z.uuid() });
 }
 
