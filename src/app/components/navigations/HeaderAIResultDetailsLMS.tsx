29099834dad0db1cbe6bd0128ef167a3c38f8e26
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2026-03-14 00:36:36 +0700

    [FE] Add Navigation Menu Profile

--- src/app/components/navigations/HeaderAIResultDetailsLMS.tsx
+++ src/app/components/navigations/HeaderAIResultDetailsLMS.tsx
@@ -1,60 +1,18 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import { Blocks, Compass, Wallet } from "lucide-react";
-import AppDropdown from "../elements/AppDropdown";
-import AppDropdownItemList from "../elements/AppDropdownItemList";
+import React from "react";
 import AvatarBadgeLMS, { AvatarBadgeLMSProps } from "../buttons/AvatarBadgeLMS";
 import AppBreadcrumb from "./AppBreadcrumb";
 import AppBreadcrumbItem from "./AppBreadcrumbItem";
 
 export interface HeaderAIResultDetailsLMSProps extends AvatarBadgeLMSProps {
-  sessionUserRole: number;
   headerResultName: string;
   headerTitle: string;
   headerDescription?: string;
 }
 
-export default function HeaderAIResultDetailsLMS({
-  sessionUserName,
-  sessionUserAvatar,
-  sessionUserRole,
-  headerTitle,
-  headerDescription,
-}: HeaderAIResultDetailsLMSProps) {
-  const [isActionsOpened, setIsActionsOpened] = useState(false);
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  // Open and close dropdown
-  const handleActionsDropdown = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-    setIsActionsOpened((prev) => !prev);
-  };
-
-  // Close dropdown if clicked outside
-  useEffect(() => {
-    const handleClickOutside = (
-      event: MouseEvent | (MouseEvent & { target: Node })
-    ) => {
-      if (
-        wrapperRef.current &&
-        !wrapperRef.current.contains(event.target as Node)
-      ) {
-        setIsActionsOpened(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  // Domain Logic
-  let domain = "sevenpreneur.com";
-  if (process.env.NEXT_PUBLIC_DOMAIN_MODE === "local") {
-    domain = "example.com:3000";
-  }
-
+export default function HeaderAIResultDetailsLMS(
+  props: HeaderAIResultDetailsLMSProps,
+) {
   return (
     <React.Fragment>
       <div className="header-root flex sticky w-full top-0 left-0 px-0 py-5 items-center justify-center bg-section-background z-40">
@@ -70,51 +28,20 @@ export default function HeaderAIResultDetailsLMS({
             </div>
             <div className="header-information flex flex-col gap-2">
               <h1 className="header-title font-brand font-bold text-2xl">
-                {headerTitle}
+                {props.headerTitle}
               </h1>
-              {headerDescription && (
+              {props.headerDescription && (
                 <p className="font-bodycopy font-medium text-[#333333]">
-                  {headerDescription}
+                  {props.headerDescription}
                 </p>
               )}
             </div>
           </div>
-          <div
-            className="user-menu relative flex hover:cursor-pointer"
-            ref={wrapperRef}
-            onClick={handleActionsDropdown}
-          >
-            <AvatarBadgeLMS
-              sessionUserAvatar={sessionUserAvatar}
-              sessionUserName={sessionUserName || "Unknown"}
-            />
-            <AppDropdown
-              isOpen={isActionsOpened}
-              onClose={() => setIsActionsOpened(false)}
-              alignDesktop="right"
-            >
-              {sessionUserRole !== 3 && (
-                <Link href={`https://admin.${domain}`}>
-                  <AppDropdownItemList
-                    menuIcon={<Blocks className="size-4" />}
-                    menuName="Dashboard Admin"
-                  />
-                </Link>
-              )}
-              <Link href={`https://www.${domain}`}>
-                <AppDropdownItemList
-                  menuIcon={<Compass className="size-4" />}
-                  menuName="Discovery"
-                />
-              </Link>
-              <Link href={`https://www.${domain}/transactions`}>
-                <AppDropdownItemList
-                  menuIcon={<Wallet className="size-4" />}
-                  menuName="Transaction"
-                />
-              </Link>
-            </AppDropdown>
-          </div>
+          <AvatarBadgeLMS
+            sessionUserAvatar={props.sessionUserAvatar}
+            sessionUserName={props.sessionUserName || "Unknown"}
+            sessionUserRole={props.sessionUserRole}
+          />
         </div>
       </div>
     </React.Fragment>
