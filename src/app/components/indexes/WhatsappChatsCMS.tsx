6aa1f4d7108830d11859694990a897a19b5b0af5
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2026-03-24 13:50:39 +0700

    [FE] Add Lead Details

--- src/app/components/indexes/WhatsappChatsCMS.tsx
+++ src/app/components/indexes/WhatsappChatsCMS.tsx
@@ -1,10 +1,21 @@
 "use client";
+import { Slider } from "@/components/ui/slider";
 import { LeadStatus } from "@/lib/app-types";
-import { ChevronRight } from "lucide-react";
+import {
+  ChevronRight,
+  CloudSync,
+  Loader,
+  PenBox,
+  TextAlignStart,
+} from "lucide-react";
+import Image from "next/image";
+import AppButton from "../buttons/AppButton";
 import WhatsappConvItemCMS from "../items/WhatsappConvItemCMS";
+import LeadStatusLabelCMS from "../labels/LeadStatusLabelCMS";
 import AppBreadcrumb from "../navigations/AppBreadcrumb";
 import AppBreadcrumbItem from "../navigations/AppBreadcrumbItem";
 import PageTitleSectionCMS from "../titles/PageTitleSectionCMS";
+import TextAreaCMS from "../fields/TextAreaCMS";
 
 export default function WhatsappChatsLMS() {
   const conv = [
@@ -130,6 +141,8 @@ export default function WhatsappChatsLMS() {
     },
   ];
 
+  const leadStatus: LeadStatus = "HOT";
+
   return (
     <div className="root hidden w-full h-screen justify-center bg-white py-8 lg:flex lg:pl-64">
       <div className="index flex flex-col max-w-[calc(100%-4rem)] w-full h-full gap-4">
@@ -145,7 +158,7 @@ export default function WhatsappChatsLMS() {
           </div>
         </div>
         <div className="flex w-full min-h-0">
-          <div className="conversations-column flex flex-col flex-1 min-h-0 shrink-0 border rounded-l-lg overflow-y-auto">
+          <div className="chat-panels flex flex-col flex-1 min-h-0 shrink-0 border rounded-l-lg overflow-y-auto">
             <div className="column-title sticky inset-0 top-0 p-3 bg-white font-bodycopy font-bold border-b z-30">
               Chats
             </div>
@@ -164,7 +177,106 @@ export default function WhatsappChatsLMS() {
             </div>
           </div>
           <div className="flex flex-[1.6] border"></div>
-          <div className="flex flex-1 border rounded-r-lg"></div>
+          <div className="lead-informations flex flex-col flex-1 w-full gap-4 p-3 py-5 border rounded-r-lg overflow-y-auto">
+            <div className="lead-identity flex flex-col items-center gap-2">
+              <div className="lead-avatar size-24 rounded-full overflow-hidden">
+                <Image
+                  src={"https://i.pravatar.cc/150?img=1"}
+                  alt="user"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <p className="lead-name text-lg text-center font-bold font-bodycopy leading-snug line-clamp-2">
+                  Budi Santoso
+                </p>
+                <p className="lead-phone-number text-sm text-[#333333]/70 font-semibold font-bodycopy leading-snug line-clamp-1">
+                  +6299898988787
+                </p>
+                <p className="lead-email text-sm text-[#333333]/70 font-semibold font-bodycopy leading-snug line-clamp-1">
+                  budisantoso@gmail.com
+                </p>
+              </div>
+              <AppButton size="smallRounded" variant="cmsPrimaryLight">
+                <CloudSync className="size-4" />
+                Sync to Database
+              </AppButton>
+            </div>
+            <div className="lead-details flex flex-col gap-2 p-3 bg-section-background/50  border border-outline rounded-md">
+              <div className="flex w-full justify-between items-center leading-snug">
+                <h5 className="font-bodycopy text-[15px] font-bold">
+                  Lead Details
+                </h5>
+                <AppButton
+                  className="text-cms-primary"
+                  size="small"
+                  variant="ghost"
+                >
+                  <PenBox className="size-3.5" />
+                  Edit
+                </AppButton>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="flex w-full items-center">
+                  <p className="label w-32 text-sm text-alternative font-bodycopy font-medium">
+                    Handled by
+                  </p>
+                  <div className="flex w-full">
+                    <div className="input flex w-fit items-center gap-2 bg-white py-1 px-2 rounded-full border border-outline">
+                      <div className="size-5 rounded-full overflow-hidden">
+                        <Image
+                          src={"https://i.pravatar.cc/150?img=1"}
+                          alt="user"
+                          width={500}
+                          height={500}
+                        />
+                      </div>
+                      <p className="text-sm text-[#333333] font-bodycopy font-semibold">
+                        Devin Suhartono
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex w-full items-center">
+                  <p className="label w-32 text-sm text-alternative font-bodycopy font-medium">
+                    Status
+                  </p>
+                  <div className="input w-full">
+                    <LeadStatusLabelCMS variants={leadStatus} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="lead-progress flex flex-col gap-2 p-3 bg-section-background/50  border border-outline rounded-md">
+              <div className="flex items-center gap-2">
+                <Loader className="size-4 text-alternative" />
+                <h5 className="font-bodycopy text-[15px] font-bold">
+                  Winning Rate
+                </h5>
+              </div>
+              <div className="flex w-full items-center gap-2">
+                <Slider defaultValue={[0]} max={100} step={5} />
+                <div className="input w-fit text-sm text-[#333333] font-bodycopy font-medium">
+                  50%
+                </div>
+              </div>
+            </div>
+            <div className="notes flex flex-col gap-2 p-3 bg-section-background/50 border border-outline rounded-md">
+              <div className="flex items-center gap-2">
+                <TextAlignStart className="size-4 text-alternative" />
+                <h5 className="font-bodycopy text-[15px] font-bold">Notes</h5>
+              </div>
+              <TextAreaCMS
+                textAreaId="cohort-description"
+                textAreaPlaceholder="Tell us about this program"
+                textAreaHeight="h-32"
+                value={""}
+                // onTextAreaChange={handleInputChange("cohortDescription")}
+                required
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
